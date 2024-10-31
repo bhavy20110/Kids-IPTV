682891c1fa553ecb81a8fd9d31a55f325cd058a2
StevoSure123 <stevosure123@gmail.com>
2024-10-31 20:06:02 +0600

    Update player.js

--- player.js
+++ player.js
@@ -1,22 +1,22 @@
 const urlParams = new URLSearchParams(window.location.search);
-const streamUrl = urlParams.get('url');
+const videoSrc = urlParams.get('url');
+const channelName = urlParams.get('name');
 
-const videoPlayer = document.getElementById('videoPlayer');
+document.getElementById('channel-name').textContent = `Playing: ${channelName}`;
 
-// Check for HLS support
 if (Hls.isSupported()) {
+    const video = document.getElementById('video');
     const hls = new Hls();
-    hls.loadSource(streamUrl);
-    hls.attachMedia(videoPlayer);
-    hls.on(Hls.Events.MANIFEST_PARSED, function() {
-        videoPlayer.play();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        video.play();
     });
-} else if (videoPlayer.canPlayType('application/vnd.apple.mpegurl')) {
-    // If the browser is Safari
-    videoPlayer.src = streamUrl;
-    videoPlayer.addEventListener('loadedmetadata', function() {
-        videoPlayer.play();
+} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = videoSrc;
+    video.addEventListener('loadedmetadata', function () {
+        video.play();
     });
 } else {
-    alert('Error: Unable to play the stream. Please check the link.');
+    alert("Error: Unable to play the stream. Please check the link.");
 }
