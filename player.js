3319e68573e6f5a051b795ceb1e26ca322427204
StevoSure123 <stevosure123@gmail.com>
2024-10-31 15:47:49 +0600

    Update player.js

--- player.js
+++ player.js
@@ -2,12 +2,21 @@ const urlParams = new URLSearchParams(window.location.search);
 const streamUrl = urlParams.get('url');
 
 const videoPlayer = document.getElementById('videoPlayer');
-videoPlayer.src = streamUrl;
 
-// Initialize Video.js
-const player = videojs(videoPlayer);
-
-// Add error handling
-player.on('error', function() {
+// Check for HLS support
+if (Hls.isSupported()) {
+    const hls = new Hls();
+    hls.loadSource(streamUrl);
+    hls.attachMedia(videoPlayer);
+    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        videoPlayer.play();
+    });
+} else if (videoPlayer.canPlayType('application/vnd.apple.mpegurl')) {
+    // If the browser is Safari
+    videoPlayer.src = streamUrl;
+    videoPlayer.addEventListener('loadedmetadata', function() {
+        videoPlayer.play();
+    });
+} else {
     alert('Error: Unable to play the stream. Please check the link.');
-});
+}
